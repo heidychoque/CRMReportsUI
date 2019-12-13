2e892611b4f1e536aa68654695e5ccaec5c82abd
heidy choque ledezma <kete_heidy@hotmail.com>
2019-12-13 03:56:36 -0400

    develop: axios services added and views modified

--- src/views/PendingQuotes.vue
+++ src/views/PendingQuotes.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="pendingQuotes">
     <h1>Pending Quotes</h1>
+    <QuotesReport/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+   
+  }
+})
+</script>
--- src/views/SoldQuotes.vue
+++ src/views/SoldQuotes.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="soldQuotes">
     <h1>Sold Quotes</h1>
+    <QuotesReport/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+ 
+  }
+})
+</script>
