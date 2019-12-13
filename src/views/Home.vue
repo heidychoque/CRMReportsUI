0703dc090b051ce72651468b3e30dd6436b72a85
heidy choque ledezma <kete_heidy@hotmail.com>
2019-12-13 04:08:15 -0400

    develop: component added with routers

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>This is the Home page</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {}
+}
+</script>
--- src/views/PendingQuotes.vue
+++ src/views/PendingQuotes.vue
@@ -7,10 +7,11 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import QuotesReport from '../components/QuotesReport.vue'
 
 export default Vue.extend({
   components: {
-   
+   QuotesReport
   }
 })
 </script>
